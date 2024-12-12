b40d9d2bc6effdc135f2262b84d1352097b2c1a9
Nico Schmidt <git@tebosbrime.de>
2024-12-12 20:18:37 +0100

    Add retry to reqwest client

--- src/blitzer_api_client.rs
+++ src/blitzer_api_client.rs
@@ -1,13 +1,16 @@
-use reqwest::{ClientBuilder};
+use reqwest_middleware::{ClientBuilder};
+use reqwest_retry::{RetryTransientMiddleware, policies::ExponentialBackoff};
 use crate::model::{ApiResponse, BlitzerClientRequestParams};
 
 const BASE_URL: &str = "https://cdn2.atudo.net/api/4.0/pois.php";
 
 pub async fn get_blitzer_api_result(client_params: BlitzerClientRequestParams) -> anyhow::Result<ApiResponse> {
-    let reqwest_client = ClientBuilder::default().build()?;
+    let retry_policy = ExponentialBackoff::builder().build_with_max_retries(3);
+    let reqwest_client = ClientBuilder::new(reqwest::Client::new())        
+        .with(RetryTransientMiddleware::new_with_policy(retry_policy))
+        .build();
 
-    let url= format!("{}{}", BASE_URL, client_params.get_request_params());
-    //println!("url: {}", url);
+    let url = format!("{}{}", BASE_URL, client_params.get_request_params());
     let response = reqwest_client
         .get(url)
         .send().await?;
